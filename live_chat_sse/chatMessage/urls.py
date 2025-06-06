585141d825fd6ee22fed8bebe4864bc0d9894bc0
kishoun <kellybrener66@gmail.com>
2025-06-06 12:44:17 +0200

    chat_bull_and_fake_users

--- live_chat_sse/chatMessage/urls.py
+++ live_chat_sse/chatMessage/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('send/', views.send_message, name='send_message'),
-	path('stream/', views.sse_view, name='sse_stream'),
+	path('stream/', views.sse_view, name='sse'),
 	path('', views.chat_page, name='chat_page'),
 ]
--- live_chat_sse/chatMessage/views.py
+++ live_chat_sse/chatMessage/views.py
@@ -19,23 +19,32 @@ latest_messages = []
 
 #à remettre après les tests
 @csrf_exempt  # désactive la sécu par défaut de django qui protège des falsifications de requêtes
-
+# @login_required
 def send_message(request):
     if request.method == "POST":
         try:
             data = json.loads(request.body)
+            username = data.get("as")
+            sender = User.objects.get_or_create(username=username)[0]
             content = data.get("content")
-            sender = request.user
-            message = Message.objects.create(sender=sender, content=content)
+			 # essayer de récupérer recipient si donné
+            recipient_username = data.get("recipient")
+            if recipient_username:
+                recipient = User.objects.get_or_create(username=recipient_username)[0]
+            else:
+                recipient = None  # message public
+            message = Message.objects.create(sender=sender, recipient=recipient, content=content)
             # Ajouter au buffer SSE
             latest_messages.append({
                 "sender": sender.username,
+                "recipient": recipient.username if recipient else "Tous",
                 "content": content,
                 "timestamp": message.timestamp.strftime("%Y-%m-%d %H:%M:%S")
                 })
             return JsonResponse({
                 "status": "success",
                 "sender": sender.username,
+                "recipient": recipient.username if recipient else None,
                 "content": content,
                 "timestamp": message.timestamp.strftime("%Y-%m-%d %H:%M:%S"),
             })
@@ -44,11 +53,11 @@ def send_message(request):
 
     return JsonResponse({"status": "error", "message": "POST only"}, status=405)
 
-@login_required
+# @login_required
 def chat_page(request):
     return render(request, "chatMessage/chat.html")
 
-@login_required
+# @login_required
 def sse_view(request):
     def event_stream():
         last_index = 0
